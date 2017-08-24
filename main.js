daef3f8343323261c6b1f2bbe405617810d97e63
K1maru <31213915+K1maru@users.noreply.github.com>
2017-08-24 16:11:44 +0200

    Add files via upload
    Added LevelUp function and function to accept next round

--- main.js
+++ main.js
@@ -2,12 +2,24 @@
 //SET the range of randomized EnemyStats here:
 var enemyNames = ["Ryu", "Chen", "Jackie", "Admiral General Aladin", "Benny the Retard"];
 var maximumHealthPossible = 40;
-var maximumStrenghtPossible = 8;
-var maximumDefPossible = 8;
-var maximumLuckPossible = 8;
+var maximumStrenghtPossible = 6;
+var maximumDefPossible = 5;
+var maximumLuckPossible = 6;
 
+//Helpers for the Enemy AI
+var lightAttackCount = 0;
+var desperateAttackCount = 0;
+var defenseCount = 0;
+
+//Might be usefull in the future
 var roundCount = 0;
+var fightCount = 1;
 
+//Need to store this somewhere
+var enemy;
+var keepOnFighting = false;
+
+//need to move the MoveProperty into the Fighter Constructor
 var yourMove;
 var enemyMove;
 
@@ -48,9 +60,9 @@ function enemyNameRandomizer() {
 function enemyHealthRandomizer() {
     var enemyHP = Math.round(Math.random() * maximumHealthPossible);
     while (enemyHP < (maximumHealthPossible / 2)) {
-        enemyHP = Math.round(Math.random() * maximumHealthPossible);
+        enemyHP = Math.round(Math.random() * maximumHealthPossible) + 10;
     }
-    return enemyHP;
+    return enemyHP + 10;
 }
 
 function enemyStrenghtRandomizer() {
@@ -77,47 +89,67 @@ function enemyDefenseRandomizer() {
 function criticalHitRate(probability) {
     var critrate = randomNumber() * 100 + probability * randomNumber();
     if (critrate > 95) {
-        window.alert("Crit!");
+        window.alert("A critical!");
         return 2;
     } else {
         return 1;
     }
 }
 
-function enemyMoveRandomizer() {
-    var switchBetweenEnemyMoves = Math.round(randomNumber() * 2);
-    switch (switchBetweenEnemyMoves) {
-    case 0:
+function callForEnemyAI() {
+    
+    if(defenseCount > lightAttackCount && defenseCount > desperateAttackCount) {
         enemyMove = "Light Attack"
         return enemyMove;
-    case 1:
+    }
+    else if(lightAttackCount > defenseCount && lightAttackCount > desperateAttackCount) {
         enemyMove = "Desperate Attack";
         return enemyMove;
-    case 2:
+    }
+    else if(desperateAttackCount > defenseCount && desperateAttackCount > lightAttackCount) {
         enemyMove = "Defend";
         return enemyMove;
     }
 }
 
-window.alert("You'll take part in a fighting tournament!")
+function enemyMoveRandomizer() {
+    
+    if(!callForEnemyAI()){
+        var switchBetweenEnemyMoves = Math.round(randomNumber() * 2);
+        switch (switchBetweenEnemyMoves) {
+        case 0:
+                enemyMove = "Light Attack"
+                return enemyMove;
+        case 1:
+                enemyMove = "Desperate Attack";
+            return enemyMove;
+        case 2:
+            enemyMove = "Defend";
+            return enemyMove;
+        }
+    }
+    else{
+        return enemyMove; 
+    }   
+}
+
+window.alert("You'll take part in a fighting tournament!");
+
 var you = new Fighter(whatIsYourName(), 50, 5, 1, 1);
+
 window.alert("Your Fighter is named " + you.name + 
              ". \nYour starting stats are:\nHP: " + you.hp +
              "\nSTR: " + you.str + 
              "\nDEF: " + you.def + 
              "\nLUK: " + you.luck);
 
-var enemy = new Fighter(enemyNameRandomizer(), 
+function callForAChallenger() {
+    enemy = new Fighter(enemyNameRandomizer(), 
                         enemyHealthRandomizer(), 
                         enemyStrenghtRandomizer(), 
                         enemyDefenseRandomizer(), 
                         enemyLuckRandomizer());
-
-window.alert("Your first enemy is named " + enemy.name + 
-             ". \nHis stats are:\nHP: " + enemy.hp +
-             "\nSTR: " + enemy.str + 
-             "\nDEF: " + enemy.def + 
-             "\nLUK: " + enemy.luck);
+}
 
 
 function attackDamageCalculation(name, attackform, str, luck) {
@@ -136,22 +168,34 @@ function attackDamageCalculation(name, attackform, str, luck) {
 }
 
 
-//WIP and not used at the moment(Need Buttons for that and have to study CSS & HTML first I guess...)
 function yourMoveChoice() {
     if (roundCount === 0) {
-        yourMove = window.prompt("What is your first Move? Light Attack, Desperate Attack or Defend?");
+        yourMove = window.prompt("What is your first Move? \n1: Light Attack \n2: Desperate Attack \n3: Defend");
     } else if (roundCount > 0) {
-        yourMove = window.prompt("What will you do next? Light Attack, Desperate Attack or Defend?");
+        yourMove = window.prompt("What will you do next? \n1: Light Attack \n2: Desperate Attack \n3: Defend");
     }
-     
+    
+    if(yourMove === "1" || yourMove === "light Attack"){
+        yourMove = "Light Attack";
+    } 
+    else if (yourMove === "2" || yourMove === "desperate attack"){
+        yourMove = "Desperate Attack";
+    }
+    else if (yourMove === "3" || yourMove === "defend"){
+        yourMove = "Defend";
+    }
+    
     switch (yourMove) {
     case "Light Attack":
-        roundCount++;
+        lightAttackCount +=1;
+        roundCount +=1;
         return yourMove;
     case "Desperate Attack":
+        desperateAttackCount +=1;
         roundCount++;
         return yourMove;
     case "Defend":
+        defenseCount +=1;
         roundCount++;
         return yourMove;
     }
@@ -201,17 +245,17 @@ function damagePhase() {
     else if (yourMove === "Defend") {
         if (enemyMove === "Light Attack") {
             you.hp = Math.round(you.hp - enemyAttack * 0.5);
-            window.alert(you.name + " defends himself and could weaken " + enemy.name + "s " + enemyMove + "(" + Math.round(you.hp - enemyAttack * 0.5) + "DMG) and so did " + enemy.name + "(" + enemyAttack + "DMG)!");
+            window.alert(you.name + " defends himself and could weaken " + enemy.name + "s " + enemyMove + "(" + Math.round(enemyAttack * 0.5) + "DMG)!");
         } else if (enemyMove === "Desperate Attack") {
             enemy.hp = enemy.hp - enemyAttack;
-            window.alert(you.name + " takes a defensive stance and counters " + enemy.name + "s " + enemyMove + " (" + enemyAttack + "DMG!) back to him");
+            window.alert(you.name + " takes a defensive stance and counters " + enemy.name + "s " + enemyMove + " (" + enemyAttack + "DMG!) back to him!");
         } else if (enemyMove === "Defend") {
             window.alert(you.name + " and " + enemy.name + " stared each other in the eyes while catching a breath and thinking about their next moves");
         }
     }
     else {
         you.hp = you.hp - 10;
-        window.alert("It was a bad from " + you.name + "idea trying to invent a new Move called " + yourMove + "in the middle of the fight. " + you.name + " recieved a hard hit (10DMG) while doing so!")
+        window.alert("It was a bad from " + you.name + "idea trying to invent a new Move " + yourMove + " in the middle of the fight. " + you.name + " recieved a hard hit (10DMG) while doing so!")
     } 
         
     
@@ -224,17 +268,66 @@ function damagePhase() {
     window.alert("You have " + you.hp + "HP left. " + enemy.name + " has " + enemy.hp + "HP left.");
 }
 
+function nextFight() {
+    keepOnFighting = window.prompt("Do you wish to fight on?").toLowerCase();
+    
+    if(keepOnFighting === "yes" || keepOnFighting === "y") {
+    keepOnFighting = true;
+    levelUpCheck();
+    window.alert("Here comes a new Challenger");
+    return keepOnFighting;
+    }
+    else {
+    window.alert(you.name + " was to frightened to keep on fighting, so he ran away like a chicken!");
+    keepOnFighting = false;
+    return keepOnFighting;
+    }
+}
+
 function fightSequence() {
     do {
         damagePhase();
     } while (you.hp > 0 && enemy.hp > 0);
     if (enemy.hp <= 0 && you.hp <= 0) { 
-        window.alert("DOUBLE KO!"); 
+        window.alert("DOUBLE KO! The tournament is over for " + you.name + " and " + enemy.name + " aswell, in Round " + fightCount + ". Fin.");
+        keepOnFighting = false;
     } else if (you.hp <= 0) { 
-        window.alert("You lose!"); 
+        window.alert(you.name + " lost VS " + enemy.name + " in Round " + fightCount + ". Fin.");
+        keepOnFighting = false;
     } else if (enemy.hp <= 0) { 
-        window.alert("You win!"); 
+        window.alert(you.name + " won VS " + enemy.name + " in Round " + fightCount); 
+        nextFight();
     }
 }
 
-fightSequence();
+function levelUpCheck() {
+    if(roundCount > fightCount * 2 && keepOnFighting == true){
+        window.alert(you.name + " leveled up through his fighting experience!!!");
+        you.hp = you.hp + 10;
+        you.str = you.str + Math.ceil(randomNumber()*2);
+        you.def = you.def + Math.ceil(randomNumber()*2);
+        you.luck = you.luck + Math.ceil(randomNumber()*2);
+        
+        window.alert(you.name + "s new stats are:\nHP: " + you.hp +
+             "(10HP recovered)\nSTR: " + you.str + 
+             "\nDEF: " + you.def +
+             "\nLUK: " + you.luck);
+    }
+}
+
+function tournament(){
+    do{
+    callForAChallenger();
+    window.alert("Your enemy is named " + enemy.name + 
+             ". \nHis stats are:\nHP: " + enemy.hp +
+             "\nSTR: " + enemy.str + 
+             "\nDEF: " + enemy.def + 
+             "\nLUK: " + enemy.luck);
+    
+    fightSequence();
+    fightCount +=1;
+    }while(keepOnFighting == true);
+    
+}
+
+tournament();
